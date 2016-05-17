d433c2a7f718808037fa0bc7545be35145baa240
George stallard <Georgestallard@students.tretherras.net>
2016-05-17 15:01:56 +0100

    Added more test for the Vertex class

--- Tests/VertexTest.java
+++ Tests/VertexTest.java
@@ -12,6 +12,59 @@ public class VertexTest {
 
         assertNull(v.getLabel());
     }
+    @Test(expected = EdgeNeighbourMissingException.class)
+    public void it_will_throw_Exception_if_a_neighbour_that_is_not_present_is_removed(){
+        Vertex v1 = new Vertex("Plymouth");
+        Vertex v2 = new Vertex("Donald");
+        Edge e1 = new Edge(v1,v2,19);
+        v1.removeNeighbour(e1);
+    }
+    @Test
+    public void It_can_count_neighbours() {
+        Vertex v1 = new Vertex("London");
+        Vertex v2 = new Vertex("Exeter");
+        Vertex v3 = new Vertex("Newquay");
+        Vertex v4 = new Vertex("Plymouth");
+        Edge e2 = new Edge(v1, v3, 10);
+        Edge e3 = new Edge(v1, v4, 12);
+        Edge e1 = new Edge(v1, v2, 5);
+        v1.addNeighbour(e1);
+        v1.addNeighbour(e2);
+        v1.addNeighbour(e3);
+        assertTrue(v1.getNeighbourCount() == 3);
+    }
+    @Test
+    public void It_can_get_a_Neighbourhood() {
+        Vertex v1 = new Vertex("London");
+        Vertex v2 = new Vertex("Exeter");
+        Vertex v3 = new Vertex("Newquay");
+        Vertex v4 = new Vertex("Plymouth");
+        Edge e2 = new Edge(v1, v3, 10);
+        Edge e3 = new Edge(v1, v4, 12);
+        Edge e1 = new Edge(v1, v2, 5);
+        v1.addNeighbour(e1);
+        v1.addNeighbour(e2);
+        v1.addNeighbour(e3);
+        assertTrue(v1.getNeighbourhood().contains(e1)&& v1.getNeighbourhood().contains(e2)&&v1.getNeighbourhood().contains(e3));
+    }
+    @Test
+    public void It_can_remove_a_neighbour(){
+        Vertex v1 = new Vertex("London");
+        Vertex v2 = new Vertex("Exeter");
+        Edge e1 = new Edge(v1,v2,5);
+        v1.addNeighbour(e1);
+        assertTrue(v1.containsNeighbour(e1));
+        v1.removeNeighbour(e1);
+        assertFalse(v1.containsNeighbour(e1));
+
+    }
+    @Test
+    public void it_can_Check_if_two_Vertexes_are_not_equal() {
+        Vertex v1 = new Vertex("London");
+        Vertex v2 = new Vertex("London Underground");
+
+        assertFalse(v1.equals(v2));
+    }
 
     @Test
     public void it_should_store_its_label() {
